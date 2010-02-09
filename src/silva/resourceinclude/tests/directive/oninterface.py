fe4d4eccb7d05155c020834ae6dffca053c9f7e3
Sylvain Viollon <thefunny@gmail.com>
2010-02-09 17:01:14 +0000

    Remove stupid test

--- src/silva/resourceinclude/tests/directive/oninterface.py
+++ src/silva/resourceinclude/tests/directive/oninterface.py
@@ -1,19 +1,18 @@
 # Copyright (c) 2008 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id$
-
-
 """
     We can only use resource martian directive on a layer:
-    >>> grok('silva.resourceinclude.tests.resources.directiveonobject')
+    >>> grok('silva.resourceinclude.tests.directive.oninterface')
     Traceback (most recent call last):
         ...
     GrokError: A resource can be included only on a layer.
-    
+
 """
 
 from zope.interface import Interface
 from silva.core import conf as silvaconf
+
 
 class ITestLayer(Interface):
     silvaconf.resource('styles.css')
