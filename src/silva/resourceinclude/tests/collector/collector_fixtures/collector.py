b2c7fed3f958cd1def48214953c7db1a19662d43
Sylvain Viollon <thefunny@gmail.com>
2010-07-28 10:50:13 +0000

    Reformat test directory

---
+++ src/silva/resourceinclude/tests/collector/collector_fixtures/collector.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from silva.core import conf as silvaconf
+
+class ITestLayer(IDefaultBrowserLayer):
+    silvaconf.resource('style.css')
+    silvaconf.resource('file1.js')
+    silvaconf.resource('file2.js')
+
+
